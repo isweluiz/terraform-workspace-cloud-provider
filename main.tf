62af9f9b09ad6b2cc7d1f3cd7002d7206c6cc781
Luiz Eduardo <lpereira@bandwidth.com>
2023-06-24 23:35:29 +0100

    ___

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = var.aws_main_region
-#   access_key = var.AWS_ACCESS_KEY_ID
-#   secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 
   default_tags {
     tags = {
