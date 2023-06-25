19b91ec084b22173b1a7a328e6a7b9355435e68e
Luiz Eduardo <lpereira@bandwidth.com>
2023-06-25 01:52:36 +0100

    ___

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -1,31 +1,36 @@
+# User
 resource "aws_iam_user" "terraform_gh" {
   name = "terraform-lab"
   path = "/systems/"
 
   tags = {
-    tag-key = format("%s-%s.%s.", var.team_name, var.service_name, var.env)
+    owner   = var.team_name
+    service = var.service_name
   }
 }
 
-resource "aws_iam_access_key" "lab" {
-  user = aws_iam_user.terraform_gh.name
-}
+# resource "aws_iam_access_key" "lab" {
+#   user = aws_iam_user.terraform_gh.name
+# }
 
-data "aws_iam_policy_document" "lb_ro" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ec2:Describe*"]
-    resources = ["*"]
-  }
-}
+# resource "aws_iam_user_policy" "lb_ro" {
+#   name   = "terraform_policy_tf_lab"
+#   user   = aws_iam_user.terraform_gh.name
+#   policy = data.aws_iam_policy_document.lb_ro.json
+# }
 
-resource "aws_iam_user_policy" "lb_ro" {
+# resource "aws_iam_user_policy" "lb_ro" {
+#   name   = "terraform_policy_tf_lab"
+#   user   = aws_iam_user.terraform_gh.name
+#   policy = data.aws_iam_policy_document.s3_access.json
+# }
+
+resource "aws_iam_user_policy" "extended_policy" {
   name   = "terraform_policy_tf_lab"
   user   = aws_iam_user.terraform_gh.name
-  policy = data.aws_iam_policy_document.lb_ro.json
+  policy = data.aws_iam_policy_document.extended_policy.json
 }
 
-
-output "secret_access_key" {
-  value = aws_iam_access_key.lab.secret
-}
+# output "secret_access_key" {
+#   value = aws_iam_access_key.lab.secret
+# }
